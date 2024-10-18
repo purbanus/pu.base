10378bde9b9788e879dc83ba74f51aef3d869b83
peter <purbanus@gmail.com>
2024-10-18 22:16:30 +0200

    Generiek gemaakt

--- pu.base/src/test/java/pu/services/buffers/AbstractBufferTest.java
+++ pu.base/src/test/java/pu/services/buffers/AbstractBufferTest.java
@@ -12,12 +12,16 @@ protected void compare( byte [] a, byte [] b )
 {
 	assertEquals( " compare sizes",  a.length, b.length );
 	for ( int x = 0; x < a.length; x++ )
+	{
 		assertEquals( " compare#" + x, a[x], b[x] );
+	}
 }
 protected void compare( String aTestName, Object [] a, Object [] b )
 {
 	assertEquals( aTestName + " compare sizes",  a.length, b.length );
 	for ( int x = 0; x < a.length; x++ )
+	{
 		assertEquals( " compare#" + x, a[x], b[x] );
+	}
 }
 }
